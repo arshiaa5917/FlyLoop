59b855d07e0612aa203635076e6035e381c40529
arshiaa5917 <153107789+arshiaa5917@users.noreply.github.com>
2026-01-12 20:15:15 -0500

    Create airports.js

---
+++ assets/airports.js
@@ -0,0 +1,35 @@
+// global: window.AIRPORTS
+window.AIRPORTS = [
+  { code:"YYZ", city:"Toronto", name:"Pearson International", country:"Canada" },
+  { code:"YTZ", city:"Toronto", name:"Billy Bishop", country:"Canada" },
+  { code:"YUL", city:"Montréal", name:"Trudeau", country:"Canada" },
+  { code:"YVR", city:"Vancouver", name:"International", country:"Canada" },
+  { code:"YYC", city:"Calgary", name:"International", country:"Canada" },
+  { code:"YOW", city:"Ottawa", name:"International", country:"Canada" },
+  { code:"YHZ", city:"Halifax", name:"Stanfield", country:"Canada" },
+  { code:"YEG", city:"Edmonton", name:"International", country:"Canada" },
+
+  { code:"JFK", city:"New York", name:"John F. Kennedy", country:"USA" },
+  { code:"EWR", city:"Newark", name:"Liberty", country:"USA" },
+  { code:"LGA", city:"New York", name:"LaGuardia", country:"USA" },
+  { code:"MIA", city:"Miami", name:"International", country:"USA" },
+  { code:"FLL", city:"Fort Lauderdale", name:"Hollywood", country:"USA" },
+  { code:"MCO", city:"Orlando", name:"International", country:"USA" },
+  { code:"ATL", city:"Atlanta", name:"Hartsfield–Jackson", country:"USA" },
+  { code:"ORD", city:"Chicago", name:"O'Hare", country:"USA" },
+  { code:"LAX", city:"Los Angeles", name:"International", country:"USA" },
+  { code:"SFO", city:"San Francisco", name:"International", country:"USA" },
+
+  { code:"LHR", city:"London", name:"Heathrow", country:"UK" },
+  { code:"CDG", city:"Paris", name:"Charles de Gaulle", country:"France" },
+  { code:"AMS", city:"Amsterdam", name:"Schiphol", country:"Netherlands" },
+  { code:"FCO", city:"Rome", name:"Fiumicino", country:"Italy" },
+  { code:"MAD", city:"Madrid", name:"Barajas", country:"Spain" },
+
+  { code:"DXB", city:"Dubai", name:"International", country:"UAE" },
+  { code:"DOH", city:"Doha", name:"Hamad", country:"Qatar" },
+  { code:"ICN", city:"Seoul", name:"Incheon", country:"Korea" },
+  { code:"HND", city:"Tokyo", name:"Haneda", country:"Japan" },
+  { code:"NRT", city:"Tokyo", name:"Narita", country:"Japan" },
+  { code:"SIN", city:"Singapore", name:"Changi", country:"Singapore" }
+];
